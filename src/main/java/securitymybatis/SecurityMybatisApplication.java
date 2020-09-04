aa8324c52ab165b1c7505eced14b07d201af374f
huangjunjie <huangjunjie@saicmobility.com>
2020-09-04 18:03:06 +0800

    跨域问题解决

--- src/main/java/securitymybatis/SecurityMybatisApplication.java
+++ src/main/java/securitymybatis/SecurityMybatisApplication.java
@@ -1,13 +1,17 @@
 package securitymybatis;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class SecurityMybatisApplication {
-
+    private static final Logger log = LoggerFactory.getLogger(SecurityMybatisApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(SecurityMybatisApplication.class, args);
+        Environment env= SpringApplication.run(SecurityMybatisApplication.class, args).getEnvironment();
+       log.info("click to login http://localhost:{}/toLogin\n\tu:root\n\tp:123 ",env.getProperty("server.port","8080"));
     }
 
 }
